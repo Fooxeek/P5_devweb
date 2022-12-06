859e6b9967874e4ce264002712df10cb81e08d0a
Fooxeek <foxeek02@gmail.com>
2022-12-06 18:58:45 +0100

    install npm & add script.js

---
+++ front/js/script.js
@@ -0,0 +1,29 @@
+fetch('http://localhost:3000/api/products')
+.then(function (response) {
+    return response.json();
+})
+.then(function (listKanap) {
+    let products = document.getElementById('items')
+
+    let index = 0
+
+    for (index; index < listKanap.lenght; index++) {
+        let kanape = listKanap[index];
+        
+        let kanapInformations = `
+        <a href="./product.html?id=${kanape._id}">
+            <article>
+                <img src=".../${kanape.imageUrl}" alt="${kanape.altTxt}">
+                <h3 class="productName">${kanape.name}</h3>
+                <p class="productDescription">${kanape.description}</p>
+            </article>
+        </a>
+        `;
+
+        products.innerHTML+=kanapInformations;
+    }
+})
+
+.catch(function(err) {
+    console.error(err);
+});
