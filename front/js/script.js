389045f0d1a551166e390a2dacba0182cbb50386
Fooxeek <foxeek02@gmail.com>
2022-12-13 18:32:57 +0100

    edit : script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/api/products')
+/*fetch('http://localhost:3000/api/products')
 .then(res => res.json())
 
 .then(function (listKanap) {
@@ -26,4 +26,34 @@ fetch('http://localhost:3000/api/products')
 
 .catch(function(err) {
     console.error(err);
-});
+});*/
+
+const ENDPOINT = "http://localhost:3000/api/products/";
+
+const getAllProducts = async (endpoint = ENDPOINT) => {
+    let data = await (await fetch(endpoint)).json();
+    console.log("nos produits: ", data);
+    return data;
+}
+
+const displayProducts = (kanapes) => {
+    let products = document.getElementById("items");
+    kanapes.forEach(kanape => {
+        let kanapInformations = `
+        <a href="./product.html?id=${kanape._id}">
+            <article>
+                <img src="${kanape.imageUrl}" alt="${kanape.altTxt}">
+                <h3 class="productName">${kanape.name}</h3>
+                <p class="productDescription">${kanape.description}</p>
+            </article>
+        </a>
+        `;
+        products.innerHTML+=kanapInformations;
+    })
+}
+
+const loadHomePage = async () => {
+    displayProducts(await getAllProducts());
+}
+
+loadHomePage()
