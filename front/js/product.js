f2436615cdcb251d7a0315603db4817e3c85c537
Fooxeek <foxeek02@gmail.com>
2022-12-27 13:35:35 +0100

    feat : add function product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,115 +1,17 @@
 const idProduct = new URL(window.location.href).searchParams.get("id");
-/*fetch('http://localhost:3000/api/products/' + idProduct).then(res => res.json())
-
-    .then(function (infoCanape) {
-        let canape_image = document.getElementsByClassName("item__img");
-        let canape_title = document.getElementById("title");
-        let canape_price = document.getElementById("price");
-        let canape_description = document.getElementById("description");
-
-        canape_title.innerHTML += infoCanape.name;
-        canape_price.innerHTML += infoCanape.price;
-        canape_description.innerHTML += infoCanape.description;
-
-        let infoImage = `<img src=${infoCanape.imageUrl} alt=${infoCanape.altText}></img>`;
-        canape_image[0].innerHTML = infoImage;
-
-        let canape_colors = infoCanape.colors;
-        let textColor = "";
-
-        canape_colors.forEach((canape_colors, index) => {
-            textColor = textColor + ` <option value="${canape_colors}">${canape_colors}</option>`
-        })
-
-        let colorChoice = document.getElementById("colors");
-        colorChoice.innerHTML = textColor;
-    })
-
-
-
-let addToCart = document.getElementById("addToCart");
-addToCart.addEventListener('click', function () {
-
-    let name = document.getElementById('title').textContent;
-
-    let canape = {
-        id: idProduct,
-        color: colors.value,
-        quantity: parseInt(quantity.value),
-        name: name,
-    };
-
-    if(canape.quantity == 0 ){
-        console.log("Entrez une quantité");
-    }else {
-        console.log(canape);
-    }
-
-
-    if (quantity.value >= 1 && quantity.value <= 100) {
- 
-        if (localStorage.getItem('panier')) {
-            panierStorage = JSON.parse(localStorage.getItem('panier'));
-            if (findItem(panierStorage, canape.id)) {
-                if (findColor(panierStorage, canape.color, canape.id)) {
-                    panierStorage.forEach(elem => {
-                        if (elem.id == canape.id && elem.color == canape.color) {
-                            elem.quantity += canape.quantity
-                            localStorage.setItem('panier', JSON.stringify(panierStorage));
-                            alert('Article ajouté au panier');
-                        }
-                    })
-                } else {
-                    panierStorage.push(canape);
-                    localStorage.setItem('panier', JSON.stringify(panierStorage));
-                    alert('Article ajouté au panier');
-                }
-            } else {
-                panierStorage.push(canape);
-                localStorage.setItem('panier', JSON.stringify(panierStorage));
-            }
-        } else {
-            let panierStorage = [];
-            panierStorage.push(canape);
-            localStorage.setItem('panier', JSON.stringify(panierStorage));
-            alert('Article ajouté au panier');
-            // console.log('panier vide')
-        }
-    } else {
-        alert("Veuillez choisir un nombre d'article compris entre 1 et 100")
-    }
-})*/
-
-function findItem(Array, value) {
-    let bool = false
-
-    for (elem of Array) {
-        if (elem.id == value) {
-            return bool = true
-        }
-    }
-    return bool
-};
-
-function findColor(Array, value, id) {
-    let bool = false
-
-    for (elem of Array) {
-        if (elem.color == value && elem.id == id) {
-            return bool = true
-        }
-    }
-    return bool
-};
 
 const ENDPOINT = "http://localhost:3000/api/products/";
 
+
+//fonction qui récupère le produit souhaité
 const getInfoProduct = async (endpoint = ENDPOINT) => {
     let data = await (await fetch(endpoint+idProduct)).json();
     console.log("information du produit: ", data);
     return data;
 }
 
+
+//fonction qui injecte dans l'HTML les éléments du produit
 const elementKanape = (infoCanape) => {
     let canape_image = document.getElementsByClassName("item__img");
     let canape_title = document.getElementById("title");
@@ -134,9 +36,90 @@ const elementKanape = (infoCanape) => {
     colorChoice.innerHTML = textColor;
 }
 
+const findItem = (Array, value) => {
+    let bool = false
 
-const loadproductPage = async () => {
-    elementKanape(await getInfoProduct());
+    for (elem of Array) {
+        if (elem.id == value) {
+            return bool = true
+        }
+    }
+    return bool
+};
+
+const findColor = (Array, value, id) => {
+    let bool = false
+
+    for (elem of Array) {
+        if (elem.color == value && elem.id == id) {
+            return bool = true
+        }
+    }
+    return bool
+};
+
+
+//Fonction qui ajoute dans le panier en fonction de la quantité le produit et sa couleur
+const addCart = () => {
+    let addToCart = document.getElementById("addToCart");
+    addToCart.addEventListener('click', function () {
+    
+        let name = document.getElementById('title').textContent;
+    
+        let canape = {
+            id: idProduct,
+            color: colors.value,
+            quantity: parseInt(quantity.value),
+            name: name,
+        };
+    
+        if(canape.quantity == 0 ){
+            console.log("Entrez une quantité");
+        }else {
+            console.log(canape);
+        }
+    
+        if (quantity.value >= 1 && quantity.value <= 100) {
+     
+            if (localStorage.getItem('panier')) {
+                panierStorage = JSON.parse(localStorage.getItem('panier'));
+                if (findItem(panierStorage, canape.id)) {
+                    if (findColor(panierStorage, canape.color, canape.id)) {
+                        panierStorage.forEach(elem => {
+                            if (elem.id == canape.id && elem.color == canape.color) {
+                                elem.quantity += canape.quantity
+                                localStorage.setItem('panier', JSON.stringify(panierStorage));
+                                alert('Article ajouté au panier');
+                            }
+                        })
+                    } else {
+                        panierStorage.push(canape);
+                        localStorage.setItem('panier', JSON.stringify(panierStorage));
+                        alert('Article ajouté au panier');
+                    }
+                } else {
+                    panierStorage.push(canape);
+                    localStorage.setItem('panier', JSON.stringify(panierStorage));
+                }
+            } else {
+                let panierStorage = [];
+                panierStorage.push(canape);
+                localStorage.setItem('panier', JSON.stringify(panierStorage));
+                alert('Article ajouté au panier');
+                // console.log('panier vide')
+            }
+        } else {
+            alert("Veuillez choisir un nombre d'article compris entre 1 et 100")
+        }
+    });
 }
 
+
+//fonction qui charge la page
+const loadproductPage = async () => {
+    elementKanape(await getInfoProduct());
+    addCart();
+}
+
+//Affichage du produit et de la fonction pour ajotué au panier
 loadproductPage()
