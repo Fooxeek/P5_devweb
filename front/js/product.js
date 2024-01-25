4938d7628450d47024a331f4e94552fb230c36bb
Fooxeek <foxeek02@gmail.com>
2024-01-25 22:16:26 +0400

    fix port

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,7 @@
 //On récupère l'id du canape correspondant dans le lien
 const idProduct = new URL(window.location.href).searchParams.get("id");
 
-const ENDPOINT = "http://127.0.0.1:3000/api/products/";
+const ENDPOINT = "http://127.0.0.1:3010/api/products/";
 
 //fonction qui récupère le produit souhaité
 const getInfoProduct = async (endpoint = ENDPOINT) => {
